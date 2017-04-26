e91a9fd15f213bbde1724f4318678a56f30280b3
Cris <cris@dosaygocorporation>
2017-04-26 15:47:39 +0800

    OK

---
+++ encode.js
@@ -0,0 +1,25 @@
+"use strict";
+{
+  const fs = require('fs');
+  const tree = JSON.parse( fs.readFileSync('tree.json', {encoding:'utf8'}));
+  const codes = {};
+
+  const stack = [ tree ];
+  console.log( stack );
+
+
+  while( stack.length ) {
+    const node = stack.pop();
+    if ( node.left ) {
+      stack.push( node.left );
+    }
+    if ( node.right ) {
+      stack.push( node.right );
+    }
+    if ( node.morpheme ) {
+      codes[node.morpheme] = node.code;
+    }
+  }
+
+  console.log( JSON.stringify( codes, null, 2 ) );
+}
---
+++ huffman.js
@@ -0,0 +1,29 @@
+"use strict";
+{
+  const fs = require('fs');
+  const morphemes = JSON.parse( fs.readFileSync( 'dict.json', { encoding: 'utf8' } ) );
+
+  while( morphemes.length > 1 ) {
+    const [ a, b ] = [ morphemes.pop(), morphemes.pop() ];
+    const leaf = { left : a, right : b, cover : a.cover + b.cover };
+    morphemes.push( leaf );
+    morphemes.sort( (a,b) => b.cover - a.cover );
+  }
+
+  const root = morphemes.pop();
+
+  const stack = [ { node: root, code : '' } ];
+
+  while( stack.length ) {
+    const { node, code } = stack.pop();
+    if ( node.left ) {
+      stack.push( { node : node.left, code : code + '0' } );
+    }
+    if ( node.right ) {
+      stack.push( { node : node.right, code : code + '1' } );
+    }
+    node.code = code;
+  }
+
+  console.log( JSON.stringify( root, null, 1 ) );
+}
